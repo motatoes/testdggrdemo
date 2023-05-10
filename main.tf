9bce3f76a7cfe0d40f90282bff5a7f480490c841
Mohamed Habib <moe.habib9@gmail.com>
2023-05-10 13:35:17 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,6 @@
-resource "null_resource" "test2" {
+resource "null_resource" "test" {
 }
 
-resource "null_resource" "test4" {
-}
-
-resource "null_resource" "test5" {
-
+output "test" {
+  value = "testvalue"
 }
